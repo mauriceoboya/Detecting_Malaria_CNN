20a60bba7aedc27e63822bdc44a06fca06a13836
mauriceoboya <mauriq97@gmail.com>
2024-05-03 08:51:18 +0300

    DVC data ingestion pipeline

--- src/malaria_cnn/pipeline/data_ingestion_pipeline.py
+++ src/malaria_cnn/pipeline/data_ingestion_pipeline.py
@@ -1,8 +1,8 @@
 
 from malaria_cnn.config.configuraion import ConfigurationManager
 from malaria_cnn.components.dataingestion import DataIngestion
-
-
+from malaria_cnn import logger
+STAGE1='Data Ingestion'
 
 class DataIngestionPipeline:
     def __init__(self) :
@@ -15,4 +15,16 @@ class DataIngestionPipeline:
             data_ingestion.download_file()
             data_ingestion.extract_zip_file()
         except Exception as e:
+            logger.error(f"Error in {STAGE1} stage")
             raise e
+        
+    
+if __name__=='__main__':
+    try:
+        data_ingestion_pipeline=DataIngestionPipeline()
+        logger.info(f"Starting >>> {STAGE1} << stage")
+        data_ingestion_pipeline.main()
+        logger.info(f"Completed >>> {STAGE1} << stage")
+    except Exception as e:
+        logger.error(f"Error in {STAGE1} stage")
+        raise e
